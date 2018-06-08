376d366b541f92c17dad4c724c780b3016b2f90f
eddy1699 <edgardosamame@gmail.com>
2018-06-08 14:55:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/meduz/proy/EconomiaFragment.java
@@ -0,0 +1,18 @@
+package com.example.meduz.proy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EconomiaFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_economia, container, false);
+    }
+
+}
